bff51dc2b6f0aa3ac54808bf3da610105ec09239
roxkon <konrad.rycerz@cannabishouse.eu>
2017-08-02 17:16:04 +0200

    setting controller action create

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,34 @@
 class CommentsController < ApplicationController
+  
+  def new  	
+  end
+
+  def create
+  	@user = current_user
+  	@movie = Movie.find(params[:movie_id])
+  	@comment = @user.comments.create(permit_params)
+  	@comment.movie_id = @movie.id
+
+  	if @comment.save
+  		redirect_to @movie
+  		flash.now[:succes] = "hurra"
+  	else
+  		redirect_to @movie
+  		flash.now[:error] = "error"
+  	end
+  end
+
+  def index  	
+  end
+
+  def edit  	
+  end
+
+  def update  	
+  end
+
+  private
+
+  def permit_params  	
+  end
 end
