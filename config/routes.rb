ac6f62bd5e64e9cb956c0d5c12bd9d4784cfe626
roxkon <konrad.rycerz@cannabishouse.eu>
2017-08-02 22:25:03 +0200

    update views, routes

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
     collection do
       get :export
     end
-    resources :comments, only: [:create, :delete]
+    resources :comments, only: [:create, :destroy]
   end
   resources :comments, only: [:index]
 end
